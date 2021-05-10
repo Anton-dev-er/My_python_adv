f35b2d744915b85bf4e267e5fb177cc8bea9185a
Anton2156 <akkforwork11@gmail.com>
2021-05-10 17:07:59 +0300

    python

--- Dz_1/1.3.py
+++ Dz_1/1.3.py
@@ -2,7 +2,7 @@ def func():
     while True:
         try:
             n1, n2, n3 = input("Введіть 3 числа: ").split()
-            return int(n1) + int(n2) + int(n3) - min([int(n1), int(n2), int(n3)])
+            return sum([int(n1), int(n2), int(n3)]) - min([int(n1), int(n2), int(n3)])
         except:continue
 
 
