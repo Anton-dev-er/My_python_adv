eba2786d7f51473843287f5df1e1b2e1f421a8ea
Anton2156 <akkforwork11@gmail.com>
2021-05-25 19:29:23 +0300

    [upd]

--- Dz_4/main_for_class_OE.py
+++ Dz_4/main_for_class_OE.py
@@ -4,15 +4,14 @@ from class_office_equipment import *
 def main():
     pr = Printer("Asus", 2002)
     pr.set_cartridge()
+    pr.set_cartridge()
     pr.start_work("Рисунок")
     print("\n=================")
     scan = Scanner("Philips", 2020, "A4")
-    scan.start_work()
-    scan.start_scan("Документ")
+    scan.start_work("Документ")
     print("\n=================")
     xerox = Xerox("Xerox ", 2020)
-    xerox.start_work()
-    xerox.start_xerox("Паспорт")
+    xerox.start_work("Паспорт")
 
     print("\n\n=====Test 1=====")
     w = Warehouse()
@@ -22,8 +21,8 @@ def main():
     w.show_devices()
 
     print("\n\n=====Test 2=====")
-    for i in Warehouse():
-        print(i)
+    for i in w:
+        print(i.model)
 
 
 main()
