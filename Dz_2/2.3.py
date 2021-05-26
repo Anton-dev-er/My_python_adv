96a88a8211aa2ab8c789fdf5dfee5f42b916410d
Anton2156 <akkforwork11@gmail.com>
2021-05-26 18:54:28 +0300

    []upd

--- Dz_2/2.3.py
+++ Dz_2/2.3.py
@@ -26,40 +26,37 @@ class Matrix:
         self.list_matrix = list_matrix
         self.height = len(list_matrix)
         self.width = len(list_matrix[0])
+        self.new_matrix = [[0] * self.width for i in range(self.height)]
 
     def __add__(self, other):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         if len(self.list_matrix) == len(other.list_matrix):
             for i in range(self.height):
                 for j in range(self.width):
-                    new_matrix[i][j] = self.list_matrix[i][j] + other.list_matrix[i][j]
-            return Matrix(new_matrix)
+                    self.new_matrix[i][j] = self.list_matrix[i][j] + other.list_matrix[i][j]
+            return Matrix(self.new_matrix)
         else:
             raise ValueError("Wrong value")
 
     def __sub__(self, other):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         if len(self.list_matrix) == len(other.list_matrix):
             for i in range(self.height):
                 for j in range(self.width):
-                    new_matrix[i][j] = self.list_matrix[i][j] - other.list_matrix[i][j]
-            return Matrix(new_matrix)
+                    self.new_matrix[i][j] = self.list_matrix[i][j] - other.list_matrix[i][j]
+            return Matrix(self.new_matrix)
         else:
             raise ValueError("Wrong value")
 
     def __mul__(self, number):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         for i in range(self.height):
             for j in range(self.width):
-                new_matrix[i][j] = self.list_matrix[i][j] * number
-        return Matrix(new_matrix)
+                self.new_matrix[i][j] = self.list_matrix[i][j] * number
+        return Matrix(self.new_matrix)
 
     def __truediv__(self, number):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         for i in range(self.height):
             for j in range(self.width):
-                new_matrix[i][j] = self.list_matrix[i][j] / number
-        return Matrix(new_matrix)
+                self.new_matrix[i][j] = self.list_matrix[i][j] / number
+        return Matrix(self.new_matrix)
 
     def __str__(self):
         return f"Ваш список:{(' '.join(map(str, self.list_matrix)))}"
--- Dz_3/test_hw3/test_matrix.py
+++ Dz_3/test_hw3/test_matrix.py
@@ -1,50 +1,51 @@
-import random
-
 class Matrix:
     def __init__(self, list_matrix):
         self.list_matrix = list_matrix
-        self.height = len(list_matrix)
-        self.width = len(list_matrix[0])
+        self.new_matrix = list_matrix
+        try:
+            self.height = len(list_matrix)
+            self.width = len(list_matrix[0])
+        except:
+            self.height = 0
+            self.width = len(list_matrix)
+
+        print(self.width, self.height)
 
     def __add__(self, other):
-        new_matrix = [[0] * self.width for i in range(self.height )]
-        if len(self.list_matrix) == len(other.list_matrix):
+        if self.width == other.width and self.height == other.height:
             for i in range(self.height):
                 for j in range(self.width):
-                    new_matrix[i][j] = self.list_matrix[i][j] + other.list_matrix[i][j]
-            return Matrix(new_matrix)
+                    self.new_matrix[i][j] = self.list_matrix[i][j] + other.list_matrix[i][j]
+            return Matrix(self.new_matrix)
         else:
             raise ValueError("Wrong value")
 
     def __sub__(self, other):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         if len(self.list_matrix) == len(other.list_matrix):
             for i in range(self.height):
                 for j in range(self.width):
-                    new_matrix[i][j] = self.list_matrix[i][j] - other.list_matrix[i][j]
-            return Matrix(new_matrix)
+                    self.new_matrix[i][j] = self.list_matrix[i][j] - other.list_matrix[i][j]
+            return Matrix(self.new_matrix)
         else:
             raise ValueError("Wrong value")
 
     def __mul__(self, number):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         for i in range(self.height):
             for j in range(self.width):
-                new_matrix[i][j] = self.list_matrix[i][j] * number
-        return Matrix(new_matrix)
+                self.new_matrix[i][j] = self.list_matrix[i][j] * number
+        return Matrix(self.new_matrix)
 
     def __truediv__(self, number):
-        new_matrix = [[0] * self.width for i in range(self.height )]
         for i in range(self.height):
             for j in range(self.width):
-                new_matrix[i][j] = self.list_matrix[i][j] / number
-        return Matrix(new_matrix)
+                self.new_matrix[i][j] = self.list_matrix[i][j] / number
+        return Matrix(self.new_matrix)
 
     def __str__(self):
         return f"Ваш список:{(' '.join(map(str, self.list_matrix)))}"
 
 
-def test_():
-    assert Matrix([2, 2]) == Matrix([2, 2])
-
-
+print(Matrix([1, 1]) * 5)
+# print(Matrix([1, 1]) + Matrix([1, 1]))
+# def test_m():
+#     assert str(Matrix([1, 1, 1])) == str(Matrix([1, 1, 1]))
